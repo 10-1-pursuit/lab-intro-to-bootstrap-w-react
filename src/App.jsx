3b6c2263730bf673f04aede44f6d53798ac00c61
Jesse Chong <jessechong@pursuit.org>
2023-08-15 14:41:04 -0400

    Added Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import postData from "./data/posts.json";
 import Nav from "./Components/NavBar";
+import Hero from "./Components/Hero"
+
 
 function App() {
   console.log(postData);
+
+  
   return (
     <main>
       <Nav />
+      <Hero />
     </main>
   );
 }
