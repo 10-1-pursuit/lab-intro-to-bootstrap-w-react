8031ab033289d06702e9e34253a3b85ffd17db54
Rose <i.a.rose.bk@gmail.com>
2023-08-15 12:37:18 -0400

    Added Component Folder and NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import postData from "./data/posts.json";
+import NavBar from "./Components/NavBar";
+
+
 
 function App() {
   console.log(postData);
   return (
+    <>
+    <nav> <NavBar/>
+    </nav>
     <main>
       <button className="btn">hi</button>
     </main>
+    </>
+
   );
 }
 
