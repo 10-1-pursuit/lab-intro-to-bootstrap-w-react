384961a45e96742babd7dfd4f7a0837ec8d51299
Rose <i.a.rose.bk@gmail.com>
2023-08-15 22:23:03 -0400

    Added footer component

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,69 +1,25 @@
 export default function NavBar() {
     return (
-      <nav className="">
-        <div className="">
-          <a className="">Rosier</a>
-          <button
-            className=""
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className=""></span>
-          </button>
-          <div className="" id="navbarSupportedContent">
-            <ul className="">
-              <li className="">
-                <a className="" aria-current="page">
-                  Home
+       <span> <header>
+                <a className="script-font" >TravelBlog</a>
+<nav>
+                  <a className="" >
+                    Home
                 </a>
-              </li>
-              <li className="">
-                <a className="">Featured</a>
-              </li>
-              <li className="">
-                <a
-                  className=""
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Learn More
-                </a>
-                <ul className="" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="">About</a>
-                  </li>
-                  <li>
-                    <a className="">Contact</a>
-                  </li>
-                  <li>
-                    <hr className="" />
-                  </li>
-                  <li>
-                    <a className="">Surprise me!</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <form className="">
-              <input
-                className=""
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
+                </nav>
+                 <nav><a className="">About</a>
+                 </nav>
+           
+                <input
+              className=""
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+               <button className="" type="submit">
+              Search
+            </button>
+        </header>
+        </span> 
     );
-  }
-  
+}
