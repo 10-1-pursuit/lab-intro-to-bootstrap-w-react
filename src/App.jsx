1e170391c43cf0b1b5e7a15f29e06cdefaa0a71e
keniaDE <decoteaukenia@gmail.com>
2023-09-05 09:50:27 -0400

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import postData from "./data/posts.json";
+import "./index.css"
 
-function App() {
+function App(postData) {
   console.log(postData);
   return (
-    <main>
-      <button className="btn">hi</button>
-    </main>
+
+    <>
+      <main>
+        <header>
+          <h1> <a >Travel Blog</a>   </h1>
+          <aside>
+            <a href="/about">About</a>  <br />
+            <a href="/contact">Contact</a>
+          </aside>
+
+
+
+        </header>
+
+
+        <h1>Travel is the only thing u buy that makes u richer</h1>
+
+        <div class="input-group">
+          <input type="search" class="form-control rounded" placeholder="Search post by location" aria-label="Search" aria-describedby="search-addon" />
+          <button type="button" class="btn btn-outline-primary">cancel</button>
+        </div>
+      </main>
+    </>
   );
 }
 
